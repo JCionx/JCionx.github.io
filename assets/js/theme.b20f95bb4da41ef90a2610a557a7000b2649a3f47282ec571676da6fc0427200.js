44fe0082f4ae545ab05725930e6ffe2d5c6a588e
JCionx <jcionx0@gmail.com>
2023-05-20 22:17:35 +0100

    new theme

---
+++ assets/js/theme.b20f95bb4da41ef90a2610a557a7000b2649a3f47282ec571676da6fc0427200.js
@@ -0,0 +1,1 @@
+function loadPreferredTheme(){const e=localStorage.getItem("pref-theme"),t=document.body.classList.contains("dark");e==="light"&&t?document.body.classList.remove("dark"):e==="dark"&&!t&&document.body.classList.add("dark")}loadPreferredTheme()
