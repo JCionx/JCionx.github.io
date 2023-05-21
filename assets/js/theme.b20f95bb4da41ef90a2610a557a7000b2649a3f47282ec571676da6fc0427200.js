056600dbc621cce755265edaff7250080cd6b4c5
JCionx <jcionx0@gmail.com>
2023-05-21 15:38:15 +0100

    404

--- assets/js/theme.b20f95bb4da41ef90a2610a557a7000b2649a3f47282ec571676da6fc0427200.js
+++
@@ -1,1 +0,0 @@
-function loadPreferredTheme(){const e=localStorage.getItem("pref-theme"),t=document.body.classList.contains("dark");e==="light"&&t?document.body.classList.remove("dark"):e==="dark"&&!t&&document.body.classList.add("dark")}loadPreferredTheme()
